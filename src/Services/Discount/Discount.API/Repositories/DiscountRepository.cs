b2412d0740b84b53d3cee8cb581af935c208f3e7
BIJAY KUMAR SAHOO <lipu555000@outlook.com>
2022-03-02 12:48:03 +0530

    Discount API done with docker compose, postgres, pgadmin, Dapper

---
+++ src/Services/Discount/Discount.API/Repositories/DiscountRepository.cs
@@ -0,0 +1,82 @@
+﻿using Discount.API.Entities;
+using System.Threading.Tasks;
+using Npgsql;
+using Dapper;
+using Microsoft.Extensions.Configuration;
+using System;
+
+namespace Discount.API.Repositories
+{
+    public class DiscountRepository : IDiscountRepository
+    {
+        private readonly IConfiguration _configuration;
+
+        public DiscountRepository(IConfiguration configuration)
+        {
+            _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+        }
+
+
+        public async Task<bool> CreateDiscount(Coupon coupon)
+        {
+            using var pgConnection = new NpgsqlConnection(_configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+
+            var affected =
+                await pgConnection.ExecuteAsync
+                    ("INSERT INTO Coupon (ProductName, Description, Amount) VALUES (@ProductName, @Description, @Amount)",
+                            new { ProductName = coupon.ProductName, Description = coupon.Description, Amount = coupon.Amount });
+
+            if (affected == 0)
+                return false;
+
+            return true;
+        }
+
+        public async Task<bool> DeleteDiscount(string productName)
+        {
+            using var pgConnection = new NpgsqlConnection(_configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+
+            var affected = await pgConnection.ExecuteAsync("DELETE FROM Coupon WHERE ProductName = @ProductName",
+                new { ProductName = productName });
+
+            if (affected == 0)
+                return false;
+
+            return true;
+        }
+
+        public async Task<Coupon> GetDiscount(string productName)
+        {
+            using var pgConnection = new NpgsqlConnection(
+                _configuration.GetValue<string>("DatabaseSettings:ConnectionString")
+                );
+            // Using Dapper as the ORM
+            var coupon = await pgConnection.QueryFirstOrDefaultAsync<Coupon>(
+                "SELECT * FROM Coupon WHERE ProductName=@ProductName", new { productName = productName }
+                );
+            if(coupon == null)
+                return new Coupon(){
+                    ProductName ="No Discount",
+                    Amount = 0,
+                    Description = "No Discount Desc"
+                };
+
+            return coupon;
+            
+        }
+
+        public async Task<bool> UpdateDiscount(Coupon coupon)
+        {
+            using var pgConnection = new NpgsqlConnection(_configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+
+            var affected = await pgConnection.ExecuteAsync
+                    ("UPDATE Coupon SET ProductName=@ProductName, Description = @Description, Amount = @Amount WHERE Id = @Id",
+                            new { ProductName = coupon.ProductName, Description = coupon.Description, Amount = coupon.Amount, Id = coupon.Id });
+
+            if (affected == 0)
+                return false;
+
+            return true;
+        }
+    }
+}
